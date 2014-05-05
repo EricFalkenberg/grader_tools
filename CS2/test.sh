eb0f686c949f8b8c2c0376ba98a8c26938a556cd
Kaitlin Hipkin <kah5368@rit.edu>
2014-05-05 13:06:43 -0400

    test.sh now accepts test programs with names of varying formats; also looks for output files of two possible name patterns

--- CS2/test.sh
+++ CS2/test.sh
@@ -18,9 +18,9 @@
 ##      tests/
 ##              correct_test_output_files
 ##      
-##      All test programs should be of the form "Test#.java".
+##      All test programs should be of the form "Test*.java".
 ##      The corresponding expected output files should be 
-##          of the form "Test#-out.txt" where # is the same for the test program.
+##          of the form "Test*-out.txt" where * is the same for the test program.
 ## 
 ## 2) Run script from each student directory **after successful compilation** with
 ##    "/path/to/test.sh feedback_file"
@@ -40,18 +40,21 @@ echo "TESTING STUDENT $student's SUBMISSION"
 
 # for each program in student directory, if it is a test program, run it.
 for prog in *; do
-    if [[ "$prog" =~ Test[0-9]*.java ]]; then
+    if [[ "$prog" =~ Test[0-9a-zA-Z]*.java ]]; then
         toPrint="============ test file $prog ============"
         echo "$toPrint"
         echo "$toPrint" >> "$1"
         outfile="${student}_out.txt"
         java "${prog//.java/}" > "$outfile" 2>&1
         expected_out="../tests/${prog//.java/-out.txt}"
+        alternate_out="${expected_out//-out/Output}"
         # if file containing expected test output is provided,
-        # append diff to feedback file
+        # append diff to feedback file.
         if [ -f "$expected_out" ]; then
         	diff -w -c "$outfile" "$expected_out" >> "$1"
-        # otherwise, append all output to feedback file
+        else if [ -f "$alternate_out" ]; then
+            diff -w -c "$outfile" "$alternate_out" >> "$1"
+        # if no expected output provided, append all output to feedback file.
         else
         	cat "$outfile" >> "$1"
         fi
